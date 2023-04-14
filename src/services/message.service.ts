a9da033518bf85d6c9038a4db38b57ad74f59b44
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-04-14 08:57:58 +0200

    add: new methods & base service

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -5,7 +5,6 @@ import {
   ImageMediaObject,
   InteractiveObject,
   LocationObject,
-  MessageRequestBody,
   MessageResponse,
   MessageTemplateObject,
   StickerMediaObject,
@@ -14,53 +13,91 @@ import {
 } from '../types';
 import { ComponentTypesEnum, MessageTypesEnum } from '../enums';
 import { WhatsAppOptions } from '../interfaces';
-import { HttpClient } from '../utils';
+import { BaseService } from './base.service';
 
-export class MessageService {
-  private readonly http: HttpClient;
-
-  constructor(private readonly options: WhatsAppOptions) {
-    this.http = HttpClient.createClient({
-      baseUrl: `https://graph.facebook.com/${this.options.cloudApiVersion}/${this.options.phoneNumberId}`,
-      headers: {
-        authorization: `Bearer ${this.options.accessToken}`,
-      },
-    });
+export class MessageService extends BaseService {
+  constructor(options: WhatsAppOptions) {
+    super(options);
   }
 
-  private messageWrapper<T extends MessageTypesEnum, U extends ComponentTypesEnum>(
-    type: T,
-    payload:
-      | AudioMediaObject
-      | [ContactObject]
-      | DocumentMediaObject
-      | ImageMediaObject
-      | InteractiveObject
-      | LocationObject
-      | MessageTemplateObject<U>
-      | StickerMediaObject
-      | TextObject
-      | VideoMediaObject,
-    toNumber: string,
+  async sendText(body: TextObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Text, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendAudio(body: AudioMediaObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Audio, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendContacts(body: [ContactObject], recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Contacts, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendDocument(body: DocumentMediaObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Document, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendImage(body: ImageMediaObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Image, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendVideo(body: VideoMediaObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Video, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendInteractive(body: InteractiveObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Interactive, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendLocation(body: LocationObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Location, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendSticker(body: StickerMediaObject, recipient: string, replyMessageId?: string): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Sticker, body, recipient, replyMessageId);
+
+    const { data } = await this.http.post<MessageResponse>('messages', payload);
+
+    return data;
+  }
+
+  async sendTemplate(
+    body: MessageTemplateObject<ComponentTypesEnum>,
+    recipient: string,
     replyMessageId?: string,
-  ) {
-    const body: MessageRequestBody<T> = {
-      messaging_product: 'whatsapp',
-      recipient_type: 'individual',
-      to: toNumber,
-      type,
-      [type]: payload,
-    };
-
-    if (replyMessageId) {
-      body.context = { message_id: replyMessageId };
-    }
-
-    return body;
-  }
-
-  async sendText(body: TextObject, recipient: number, replyMessageId?: string): Promise<MessageResponse> {
-    const payload = this.messageWrapper(MessageTypesEnum.Text, body, recipient.toString(), replyMessageId);
+  ): Promise<MessageResponse> {
+    const payload = this.messageWrapper(MessageTypesEnum.Template, body, recipient, replyMessageId);
 
     const { data } = await this.http.post<MessageResponse>('messages', payload);
 
